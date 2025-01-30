d079ab66e5e745c6f4f557fa56391d03ad6ecfd4
Josh K <28068117+iamjoshk@users.noreply.github.com>
2025-01-30 16:13:15 -0500

    update

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 import logging
-from homeassistant.helpers.discovery import load_platform
+from homeassistant.helpers.discovery import async_load_platform
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -7,6 +7,6 @@ async def async_setup(hass, config):
     """Set up the ZHA Device Info component."""
     _LOGGER.info("Setting up ZHA Device Info")
     hass.async_create_task(
-        load_platform(hass, 'sensor', 'zha_device_info', {}, config)
+        async_load_platform(hass, 'sensor', 'zha_device_info', {}, config)
     )
     return True
