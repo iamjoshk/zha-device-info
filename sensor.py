5d719135d32eabeb39fc6542396a55964bef3040
Josh K <28068117+iamjoshk@users.noreply.github.com>
2025-01-31 10:08:24 -0500

    update

--- sensor.py
+++ sensor.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
@@ -31,7 +32,7 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(
     hass: HomeAssistant,
-    config_entry: ConfigType,
+    entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up ZHA device info sensors from a config entry."""
